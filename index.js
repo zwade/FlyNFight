defaa5782229891297aac9432dec411e67f99172
Zach Wade <zacharywade@gmail.com>
2014-03-30 03:30:22 -0400

    example

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-var ws = require("ws")
+var WebSocketServer = require('ws').Server
+	, http = require('http')
+	, express = require('express')
+	, app = express()
+	, port = process.env.PORT || 5000;
 
-wss = new ws.Server({port: process.env.PORT || 8080});
-wss.on("connection",function() {
-	console.log("hi");
-})
+app.use(express.static(__dirname + '/'));
 
-console.log(process.env.PORT);
+var server = http.createServer(app);
+server.listen(port);
+
+console.log('http server listening on %d', port);
+
+var wss = new WebSocketServer({server: server});
+console.log('websocket server created');
+wss.on('connection', function(ws) {
+	var id = setInterval(function() {
+		ws.send(JSON.stringify(new Date()), function() {});
+	}, 1000);
+
+	console.log('websocket connection open');
+
+	ws.on('close', function() {
+		console.log('websocket connection close');
+		clearInterval(id);
+	});
+});
