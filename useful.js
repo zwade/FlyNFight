6e24556e2013f90081d35575ebc8f985abe1757d
Zach Wade <zacharywade@gmail.com>
2014-04-02 12:32:14 -0400

    hi

--- useful.js
+++ useful.js
@@ -8,18 +8,40 @@ app.use(express.static(__dirname + '/'));
 
 var server = http.createServer(app);
 server.listen(port);
-
+console.log('http server listening on %d', port);
 var wss = new WebSocketServer({server: server});
 console.log('websocket server created');
+
+var conns = {}
+
+var genUID = function() {
+	var text = "";
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for( var i=0; i < 8; i++ )
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+	return text;	
+}
+
 wss.on('connection', function(ws) {
-	var id = setInterval(function() {
-		ws.send(JSON.stringify(new Date()), function() {	});
-	}, 1000);
 
 	console.log('websocket connection open');
+	if (!ws.UID) {
+		var uid = genUID();
+		ws.UID = uid;
+		conns[uid] = ws;
+		ws.send("set uid "+uid);
+	}
+
+	ws.on('message', function(data) {
+		ws.send("data received "+data);
+	})
+	
+
 
 	ws.on('close', function() {
 		console.log('websocket connection close');
-		clearInterval(id);
+		delete conns[ws.UID]
 	});
 });
