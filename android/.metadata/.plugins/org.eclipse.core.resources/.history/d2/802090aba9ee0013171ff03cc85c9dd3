fd4eccf9bf022f3de56ca68c4e67d4ee9c279c1e
Zach Wade <zacharywade@gmail.com>
2014-06-08 12:41:14 -0400

    stuff

---
+++ android/.metadata/.plugins/org.eclipse.core.resources/.history/d2/802090aba9ee0013171ff03cc85c9dd3
@@ -0,0 +1,20 @@
+package me.zwad3.pseudosockettest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
+import android.view.View;
+
+public class PromptActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	}
+	
+	public void updateStatus(View v) {
+		Log.d("fnf","boo");
+	}
+}
--- android/PseudoSocketTest/gen/me/zwad3/pseudosockettest/R.java
+++ android/PseudoSocketTest/gen/me/zwad3/pseudosockettest/R.java
@@ -8,6 +8,9 @@
 package me.zwad3.pseudosockettest;
 
 public final class R {
+    public static final class array {
+        public static final int ship_array=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -25,16 +28,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080009;
-        public static final int button1=0x7f080003;
-        public static final int container=0x7f080000;
-        public static final int editText1=0x7f080008;
-        public static final int hostname=0x7f080002;
-        public static final int radioButton1=0x7f080006;
-        public static final int radio_ninjas=0x7f080004;
-        public static final int radio_pirates=0x7f080005;
-        public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080007;
+        public static final int action_settings=0x7f090007;
+        public static final int button1=0x7f090003;
+        public static final int container=0x7f090000;
+        public static final int editText1=0x7f090006;
+        public static final int hostname=0x7f090002;
+        public static final int ship_spinner=0x7f090004;
+        public static final int textView1=0x7f090001;
+        public static final int textView2=0x7f090005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -43,12 +44,12 @@ public final class R {
         public static final int prompt_activity=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
     }
     public static final class style {
         /** 
@@ -72,10 +73,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
